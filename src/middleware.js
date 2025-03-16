b8e66ab96badb6acdd0b1a6125d3661e488a576e
Diego Arruda <arrudadiego45@gmail.com>
2025-03-16 16:06:38 -0300

    feat

--- src/middleware.js
+++ src/middleware.js
@@ -5,16 +5,26 @@ import { cookies } from "next/headers";
 export async function middleware(req) {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
+  const tokenVerified = await verifyToken(token);
 
-  const tokenVerified = await verifyToken(token)
+  console.log(token);
+  console.log(tokenVerified);
 
-  if (!token || !tokenVerified) {
+  const { pathname } = req.nextUrl;
+  const isDashboardRoute = pathname.startsWith("/dashboards");
+  const isHomeRoute = pathname === "/";
+
+  if (isDashboardRoute && (!token || !tokenVerified)) {
     return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (isHomeRoute && token && tokenVerified) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/"], // Aplica o middleware às rotas dentro de /dashboards e à home
 };
